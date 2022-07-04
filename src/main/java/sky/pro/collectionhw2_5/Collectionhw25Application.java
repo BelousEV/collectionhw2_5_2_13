accd0d7e54baa00d779ddce236fcb229918542bf
ElenaR <llinusik@mail.ru>
2022-07-04 18:27:03 +0300

    Initial commit

---
+++ src/main/java/sky/pro/collectionhw2_5/Collectionhw25Application.java
@@ -0,0 +1,13 @@
+package sky.pro.collectionhw2_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collectionhw25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Collectionhw25Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/collectionhw2_5/Collectionhw25ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.collectionhw2_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Collectionhw25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
